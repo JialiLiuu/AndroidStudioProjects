18042ed0a9797dbcd3bee96e8698e3cfa9222d1e
E_Dazzle <oncwnuNL_cP1PSQ9gAowKJEPfXPI@git.weixin.qq.com>
2021-10-12 19:16:52 +0800

    每天更新一次汇率

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -22,12 +22,20 @@ import android.widget.Toast;
 
 import com.example.base.BaseActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.annotation.Documented;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -47,10 +55,15 @@ public class MainActivity2 extends BaseActivity implements Runnable {
         handler=new Handler(Looper.myLooper()){
             @Override
             public void handleMessage(Message message){
-                Log.i("MainActivity1","收到消息");
+                Log.i("MainActivity2","收到消息");
+
                 if(message.what==6){
-                    String str = (String) message.obj;
-                    show.setText(str);
+                    Bundle bundle = (Bundle) message.obj;
+                    dollar_rate=bundle.getFloat("dollar_rate");
+                    euro_rate=bundle.getFloat("euro_rate");
+                    won_rote=bundle.getFloat("won_rote");
+
+                    Toast.makeText(MainActivity2.this,"汇率更新",Toast.LENGTH_SHORT).show();
                 }
                 super.handleMessage(message);
             }
@@ -58,6 +71,21 @@ public class MainActivity2 extends BaseActivity implements Runnable {
 
         Thread thread = new Thread(this);
         thread.start();
+    }
+
+    public static boolean isThisTime(long time, String pattern) {
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+        String param = sdf.format(date);//参数时间
+        Log.i("MainActivity2","参数时间："+param);
+        String now = sdf.format(new Date());//当前时间
+        Log.i("MainActivity2","当前时间："+now);
+        if (param.equals(now)) {
+            Log.i("汇率已经修改","当前时间："+now);
+            return true;
+        }
+        Log.i("汇率还未修改","当前时间："+now);
+        return false;
     }
 
     private void loadFromSP(){
@@ -126,24 +154,63 @@ public class MainActivity2 extends BaseActivity implements Runnable {
             e.printStackTrace();
         }
 
-        URL url = null;
-        try{
-            url = new URL("https://www.usd-cny.com/bankofchina.htm");
-            HttpsURLConnection http = (HttpsURLConnection) url.openConnection();
-            InputStream in = http.getInputStream();
-            String html = inputStream2String(in);
-            Log.i("MainActivity1",html);
-        }catch (MalformedURLException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+//        URL url = null;
+//        try{
+//            url = new URL("https://www.usd-cny.com/bankofchina.htm");
+//            HttpsURLConnection http = (HttpsURLConnection) url.openConnection();
+//            InputStream in = http.getInputStream();
+//            String html = inputStream2String(in);
+//            Log.i("MainActivity","run: html="+html);
+//        }catch (MalformedURLException e){
+//            e.printStackTrace();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+        Bundle bundle = new Bundle();
 
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+        long time = sharedPreferences.getLong("data_2",0);
+        Log.i("MainActivity2","日期是="+time);
+
+        if(isThisTime(time,"yyyy-MM-dd")){  //是今天
+            loadFromSP();
+            bundle.putFloat("dollar_rate",dollar_rate);
+            bundle.putFloat("euro_rate",euro_rate);
+            bundle.putFloat("won_rote",won_rote);
+        }
+        else{   //不是今天
+            try {
+                Document doc = Jsoup.connect("https://usd-cny.com/").get();
+                Element firstTable = doc.getElementsByTag("table").first();
+                Elements trs = firstTable.getElementsByTag("tr");
+                trs.remove(0);
+                for(Element tr : trs){
+                    Elements tds = tr.getElementsByTag("td");
+                    Element td1 = tds.get(0);
+                    Element td2 = tds.get(4);
+//                Log.i("汇率",""+td1.text()+"------"+td2.text());
+                    if("美元".equals(td1.text())){
+                        Log.i("美元",""+td1.text()+"------"+td2.text());
+                        bundle.putFloat("dollar_rate",Float.valueOf(td2.text()));
+                    }
+                    else if("欧元".equals(td1.text())){
+                        bundle.putFloat("euro_rate",Float.valueOf(td2.text()));
+                    }
+                    else if("韩币".equals(td1.text())){
+                        bundle.putFloat("won_rote",Float.valueOf(td2.text()));
+                    }
+                }
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong("data_2",Long.valueOf(System.currentTimeMillis()));
+                editor.apply();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
         Message message = handler.obtainMessage(6);
 
-        message.obj="hello from run";
+        message.obj=bundle;
         handler.sendMessage(message);
-
     }
 
     private String inputStream2String(InputStream inputStream) throws IOException{
