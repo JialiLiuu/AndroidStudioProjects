6a489aae424115adb0f1cdf65068e7006687a9e3
E_Dazzle <oncwnuNL_cP1PSQ9gAowKJEPfXPI@git.weixin.qq.com>
2021-10-13 19:06:12 +0800

    列表显示汇率

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -3,13 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 
 import com.example.base.BaseActivity;
 
